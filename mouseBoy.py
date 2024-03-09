d4a40d3ef55e3f3b8d65ddb2de5defaa67b141c0
Leandro-Resendiz <159493735+Leandro-Resendiz@users.noreply.github.com>
2024-03-09 17:52:04 -0600

    Mouse automatico
    Movimiento del mouse de forma automatica

---
+++ mouseBoy.py
@@ -0,0 +1,15 @@
+import pyautogui as botMouse
+import random
+import time
+
+
+
+while True:
+    print(botMouse.position())
+    x= random.randint(400,900)
+    y = random.randint(100,700)
+
+
+   # botMouse.moveTo(x,y,0.5)
+    time.sleep(2)
+
